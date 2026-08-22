1c821eb67a52bb962795d7bea0dced0faecd56ce
kiikoot7 <kiikoot7@gmail.com>
2026-08-22 17:48:17 +0530

    Time: 0 ms (100%), Space: 7.7 MB (68.55%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int s = 0, p = 1;
+        int x = n;
+        
+        while (x != 0) {
+            int v = x % 10; // Extract the last digit
+            x /= 10;        // Remove the last digit
+            s += v;         // Add to digit sum
+            p *= v;         // Multiply with digit product
+        }
+        
+        // Check if n is divisible by the sum of digit sum and digit product
+        return n % (s + p) == 0;
+    }
+};
